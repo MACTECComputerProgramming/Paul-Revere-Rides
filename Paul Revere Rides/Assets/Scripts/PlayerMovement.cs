e5072ca276665b15fa1a7e5a3192be19b8d15546
Brentyn Morgan <21btmorgan@macon.k12.mo.us>
2020-10-16 19:55:03 -0700

    10/16/2020

--- Paul Revere Rides/Assets/Scripts/PlayerMovement.cs
+++ Paul Revere Rides/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody rb; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb.AddForce(200, 0, 0); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.AddForce(200, 100, 0);
     }
 }
