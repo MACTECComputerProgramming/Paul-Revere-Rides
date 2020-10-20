0d5e3af77220987019de4da80c321bf95b22f9d5
Brentyn Morgan <21btmorgan@macon.k12.mo.us>
2020-10-19 22:43:41 -0700

    10/15/2020
    Changed things on the player movement script

--- Paul Revere Rides/Assets/Scripts/PlayerMovement.cs
+++ Paul Revere Rides/Assets/Scripts/PlayerMovement.cs
@@ -3,17 +3,38 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody rb; 
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
 
     // Start is called before the first frame update
     void Start()
     {
+<<<<<<< Updated upstream
         rb.AddForce(0, 200, 500);
+=======
+      
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+<<<<<<< Updated upstream
         rb.AddForce(0, 0, 2000 * Time.deltaTime);
+=======
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce, 0, 0);
+        }
+>>>>>>> Stashed changes
     }
 }
