29366355bd5d5810ccf332e7b0c7fb2baca83501
BrentynMorgan35 <54853239+BrentynMorgan35@users.noreply.github.com>
2020-10-20 09:46:23 -0500

    10/20/2020

--- Paul Revere Rides/Assets/Scripts/PlayerMovement.cs
+++ Paul Revere Rides/Assets/Scripts/PlayerMovement.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody rb; 
+    public Rigidbody rb;
+    public float forwardForce = 1000f;
+    public float sidewayForce = 500f;
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +16,15 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewayForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0);
+        }
     }
 }
