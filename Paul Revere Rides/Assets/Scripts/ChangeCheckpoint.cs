1bfe6ca636a00b367e67c0f541ec9ab03bfb1315
Brentyn Morgan <21btmorgan@macon.k12.mo.us>
2020-10-21 21:01:00 -0700

    10/21/2020 checkpoints

--- Paul Revere Rides/Assets/Scripts/ChangeCheckpoint.cs
+++ Paul Revere Rides/Assets/Scripts/ChangeCheckpoint.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class ChangeCheckpoint : MonoBehaviour
 {
+    public GameObject checkpoint;
     // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter(Collider plyr)
     {
-        
+        if (plyr.gameObject.tag == "Player")
+            Destroy(checkpoint);
+        Destroy(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
