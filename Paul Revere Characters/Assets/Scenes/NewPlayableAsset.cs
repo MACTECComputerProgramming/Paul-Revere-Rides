0f4d3645c5b7b78caa33dde3f9e5c96067fdc340
HyEnA-2010 <70332510+HyEnA-2010@users.noreply.github.com>
2020-10-28 11:06:07 -0500

    10/28/2020

---
+++ Paul Revere Characters/Assets/Scenes/NewPlayableAsset.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.Playables;
+
+[System.Serializable]
+public class NewPlayableAsset : PlayableAsset
+{ NewPlayableAsset exttra derive(MonoBehaviour);
+
+    public NewPlayableAsset Playable1 { get => Playable; set => Playable = value; }
+    public NewPlayableAsset Extra { get => Extra1; set => Extra1 = value; }
+    public NewPlayableAsset Extra1 { get => extra; set => extra = value; }
+
+    public override Playable NewPlayableAsset(PlayableGraph graph, GameObject go)
+    {
+        prop CreatePlayable(NewPlayableAsset);
+    }
+}
