9831bca7045d52366b56be0aa67d7c45124a4a6f
Chris Robuck <22cmrobuck@macon.k12.o.us>
2020-10-30 11:06:38 -0500

    Questions added

---
+++ Paul Revere Rides/Assets/Questions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Questions : MonoBehaviour
+{
+    public Text question, ans1, ans2, ans3, ans4;
+    
+    void Start()
+    {
+        //  int rnd = Random.Range(1, 8);
+        int rnd = Random.Range(1, 1);
+
+        switch (rnd)
+        {
+            case 1:
+                question.text = "Question";
+                ans1.text = "correect";
+                ans2.text = "wrong";
+                ans3.text = "w";
+                ans4.text = "w";
+                ans1.GetComponentInParent<CorrectAns>().correct = true;
+                break;
+                
+        }
+    }
+}
